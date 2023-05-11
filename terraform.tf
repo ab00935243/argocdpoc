c27bce906be7a6857b82bf0cae3253b2f881ad67
ab00935243 <128598372+ab00935243@users.noreply.github.com>
2023-05-11 17:22:54 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,64 +1,55 @@
+# main.tf
+
+# Provider configuration for Azure
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "aks" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
+# Resource Group
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "my-aks-resource-group"
+  location = "East US"
 }
 
-resource "azurerm_virtual_network" "aks" {
-  name                = var.virtual_network_name
-  location            = azurerm_resource_group.aks.location
-  resource_group_name = azurerm_resource_group.aks.name
+# Virtual Network
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = "my-aks-vnet"
   address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
 }
 
-resource "azurerm_subnet" "aks" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.aks.name
-  virtual_network_name = azurerm_virtual_network.aks.name
-  address_prefixes     = ["10.0.0.0/24"]
+# Subnet
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.aks_rg.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["10.0.10.0/24"]
 }
 
-data "external" "aks_ssh_public_key" {
-  program = ["bash", "-c", "az aks show --resource-group ${azurerm_resource_group.aks.name} --name ${var.cluster_name} --query 'linuxProfile.ssh.publicKeys[0].keyData' -o tsv"]
-}
+# AKS Cluster
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "my-aks-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "my-aks-dns"
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.cluster_name
-  location            = azurerm_resource_group.aks.location
-  resource_group_name = azurerm_resource_group.aks.name
-  dns_prefix          = var.dns_prefix
-
-  linux_profile {
-    admin_username = var.admin_username
-
-    ssh_key {
-      key_data = data.external.aks_ssh_public_key.result
-    }
+  default_node_pool {
+    name                = "default"
+    node_count          = 1
+    vm_size             = "Standard_D2_v2"
   }
 
-  agent_pool_profile {
-    name            = "agentpool"
-    count           = var.node_count
-    vm_size         = var.vm_size
-    os_type         = "Linux"
-    vnet_subnet_id  = azurerm_subnet.aks.id
-  }
 
   service_principal {
-    client_id     = var.client_id
-    client_secret = var.client_secret
+    client_id     = "740dfa8e-9f1b-4a66-9851-a307fe754482"
+    client_secret = "YU~8Q~6_MMvmI67QfoJaKLyx3nps8RgsZw3KBbNu"
   }
 
-  depends_on = [azurerm_subnet.aks]
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "azure"
+  }
 }
 
-data "tls_private_key" "cluster_ssh" {
-  algorithm = "RSA"
-}
 
-data "tls_public_key" "cluster_ssh" {
-  private_key = data.tls_private_key.cluster_ssh.private_key_pem
-}
